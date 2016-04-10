6148bb17eaf691844730b6dd6f2918a9c1dd5cce
CadeBrown <brown.cade@gmail.com>
2016-04-09 20:10:18 -0400

    First working example

--- C/PGS.c
+++ C/PGS.c
@@ -11,6 +11,16 @@
 #include "PGSlib.h"
 
 
+//Default max coefficient to search
+#define MAX_CO 100
+
+
+//How many x values to cap at?
+#define MAX_X 100
+
+//How many primes in a notable function
+#define NOTABLE_PRIMES 5
+
 /* 
  * Copyright (C) 2016 ChemicalDevelopment
  * This program is free software: you can redistribute it and/or modify
@@ -25,17 +35,57 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+//Prints out the polynomial
+void printfPolynomial(long int p[], int l) {
+    if (l == 0) return;
+    printf("%ld", p[0]);
+    int i;
+    for (i = 1; i < l; i++) {
+        printf(" + %ldx^%d", p[i], i);
+    }
+}
+
+
+//Start generating random polynomials with length pl
+void rand_2term() {
+    long int p[2];
+    p[0] = 0;
+    p[1] = 0;
+    int p0;
+    int p1;
+    int x;
+    int primesinarow = 0;
+    int p_x;
+    for (p0 = 1; p0 <= MAX_CO; ++p0) {
+        p[0] = p0;
+        for (p1 = 1; p1 <= MAX_CO; ++p1) {
+            p[1] = p1;
+            primesinarow = 0;
+            for (x = 0; x < MAX_X; ++x) {
+                p_x = eval(p, x, 2);
+                if (isprime(p_x) == 0) { //If it is prime
+                    ++primesinarow;
+                } else {
+                    break;
+                }
+            }
+            if (primesinarow >= NOTABLE_PRIMES) {
+                printfPolynomial(p, 2);
+                printf("   is prime for the first %d values, starting at x = 0", primesinarow);
+                printf("\n");
+            }
+        }
+    }
+}
+
+
+
 
 //Main method
 int main(int argc, char *argv[]) {
-    int degree = 2;
-    long int p[degree + 1];
-    p[0] = 3;
-    p[1] = 2;
-    p[2] = 11;
-    //p(x) = 11x^2 + 2x + 3
-    long int r = eval(p, 5, degree + 1);
-    printf("%ld\n", r);
+    if (argc == 1) { //If no arguments
+        rand_2term();
+    }
 }
 
 
--- C/PGSlib.h
+++ C/PGSlib.h
@@ -7,6 +7,20 @@
 
 #include <stdio.h>
 
+//Returns 0 if prime
+int isprime(long int x) {
+    int max = (int)floor(sqrt(x));
+    max += 2;
+    int i;
+    for (i = 2; i < max; i++) {
+        if (x % i == 0) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+
 //Evaluates p(x)
 long int eval(long int p[], long int x, int terms) {
     long int sum = 0; //Our sum
