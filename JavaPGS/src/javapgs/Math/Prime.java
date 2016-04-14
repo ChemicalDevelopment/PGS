68e07f5320c303439a0ae0ebc4de032d2262de8d
CadeBrown <brown.cade@gmail.com>
2016-04-14 13:41:13 -0400

    More scripts

--- JavaPGS/src/javapgs/Math/Prime.java
+++ JavaPGS/src/javapgs/Math/Prime.java
@@ -44,7 +44,7 @@ public class Prime {
         + 101 * Math.max(Math.abs(q.b_offset), Math.abs(q.b_offset + q.b_range))
         + Math.max(Math.abs(q.c_offset), Math.abs(q.c_offset + q.c_range));
         
-        sieve(Util.forceToNearest(max_needed, 10000), 100);
+        sieve(Util.forceToNearest(max_needed, 1000), 100);
     }
     
 
