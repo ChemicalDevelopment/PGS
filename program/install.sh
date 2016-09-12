2a8e8f0cc8865dca6bb0aff4d70c3ea7ba73b80e
Cade Brown <brown.cade@gmail.com>
2016-09-12 16:13:14 -0400

    Added install script

--- program/install.sh
+++ program/install.sh
@@ -1,2 +1,4 @@
 npm install
-curl https://firebasestorage.googleapis.com/v0/b/pgsdb-c4faf.appspot.com/o/primes_2p26.dat?alt=media&token=6fb3415c-3f7b-4409-afa1-8c4fc4d7ec5e -o primes.dat
+cp ./example.prefs ./my.prefs
+cd lib
+./update.sh 1000000000
