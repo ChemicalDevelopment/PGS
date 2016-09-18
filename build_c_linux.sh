fb31f335e290df2cebf24a28b9501a55edab688c
Cade Brown <brown.cade@gmail.com>
2016-09-17 23:10:46 -0400

    UPdated zip

--- build_c_linux.sh
+++ build_c_linux.sh
@@ -9,5 +9,5 @@ cp ./lib/lib.o ./build/c/linux/lib.o
 enclose PGS.js
 cp ./PGS ./build/c/linux/PGS
 rm ./PGS
-cp ./build/c/
-zip -r ./linux-C-$1.zip ./linux
+cd ./build/c/
+zip -r "./linux-C-"$1".zip" ./linux
