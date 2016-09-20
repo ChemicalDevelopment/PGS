6f095cf72814c180b79104107ec36bc7bd14a0a8
Cade Brown <brown.cade@gmail.com>
2016-09-20 17:51:32 -0400

    UPdated install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ cd lib
 chmod +x ./update.sh
 #Generates sieve of primes
 echo "This part may take up to 2 minutes"
-./update.sh
+./lib/update.sh
 mkdir output
 touch output/output.txt
 echo "! Remember to edit ./my.prefs !"
