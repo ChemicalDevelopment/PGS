c6bab80f00b9692682f3d39baf895fb12bca7f33
Cade Brown <brown.cade@gmail.com>
2016-09-20 18:24:53 -0400

    Updated build script x4

--- build_c_linux.sh
+++ build_c_linux.sh
@@ -7,6 +7,8 @@ cd ..
 mkdir -p build/c/linux
 cp ./CPGS/CPGS.o ./build/c/linux/CPGS.o
 cp ./lib/lib.o ./build/c/linux/lib.o
+cp ./run_c.sh ./build/c/linux/run_c.sh
+#cp ./example.prefs ./build/c/linux/my.prefs
 enclose PGS.js
 cp ./PGS ./build/c/linux/PGS
 rm ./PGS
