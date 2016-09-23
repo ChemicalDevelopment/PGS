619a4f04a5dff38e11bcee2a4eef5e7b357cf472
Cade Brown <brown.cade@gmail.com>
2016-09-22 22:35:51 -0400

    Added console.log

--- PGS.js
+++ PGS.js
@@ -445,6 +445,7 @@ function shutdown() {
         rejectFuncs[f]("Shut down");
     }
     for (var i = 0; i < queue_arr.length; ++i) {
+        console.log("Shutting down queue " + i);
         queue_arr[i].shutdown();
     }
 }
