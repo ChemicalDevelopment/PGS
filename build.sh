446f4c42493537f1c739cea4864176200af98b73
Cade Brown <brown.cade@gmail.com>
2016-09-24 13:55:36 -0400

    updated build script

--- build.sh
+++ build.sh
@@ -1,34 +1,39 @@
 #!/bin/bash
-#Detect OS, and set the build os, unless they enter in the first argument
-if [[ "$1" == "" ]]; then
+
+#Node's base download
+NODE_BASE="https://nodejs.org/download/release/v5.10.1/"
+
+#Detect OS, and set the build os, unless they enter in the first argument. Use `auto` to autodetect
+if [ "$1" == "" ] || [ "$1" == "auto" ]; then
     if [[ "$OSTYPE" == "linux-gnu" ]]; then
         BUILD_OS="linux"
+        NODE_TAR="node-v5.10.1-linux-x64"
     elif [[ "$OSTYPE" == "darwin"* ]]; then
         BUILD_OS="mac"
-    elif [[ "$OSTYPE" == "cygwin" ]]; then
-        BUILD_OS="cygwin"
-    elif [[ "$OSTYPE" == "msys" ]]; then
-        BUILD_OS="win-lite"
+        NODE_TAR="node-v5.10.1-darwin-x64"
     elif [[ "$OSTYPE" == "freebsd"* ]]; then
-        BUILD_OS="freebsd"
+        echo "Error! freebsd not supported yet"
+        exit 1
+    elif [ "$OSTYPE" == "cygwin" ] || [ "$OSTYPE" == "msys" ]; then
+        echo "Error! Cygwin not supported"
+        exit 1
     else
         BUILD_OS="default"
+        echo "Warning: OS not found. Storing in ./build/default"
     fi
 else
     BUILD_OS=$1
+    NODE_TAR="node-v5.10.1-$1"
 fi
 
-#EncloseJS
-#if [[ `npm list enclose | grep -c enclose` == "0" ]]; then
-#    npm install --save enclose
-#fi
-
-#update
-./compile.sh
+echo "Building for $BUILD_OS using tar: $NODE_TAR"
 
 #Declare base dir and install dir
 BASE_DIR=$PWD
 INSTALL_DIR=$BASE_DIR/build/$BUILD_OS
+
+#Update binaries
+./compile.sh
 
 #Delete old build
 rm -rf $INSTALL_DIR
@@ -60,28 +65,22 @@ cp $BASE_DIR/node_modules/ $INSTALL_DIR/node_modules/ -rf
 #Copy the readme
 cp $BASE_DIR/RELEASE_README.txt $INSTALL_DIR/README.txt
 
-#Copy node binary in
-#if [[ "$(/usr/local/bin/node -v | grep -c v)" == "1" ]]; then
-#    cp /usr/local/bin/node $INSTALL_DIR/node
-#else
-#    cp /usr/bin/node $INSTALL_DIR/node
-#fi
-#
-mkdir $INSTALL_DIR/tmp/
-curl https://nodejs.org/download/release/v5.10.1/node-v5.10.1-linux-x64.tar.xz > $INSTALL_DIR/tmp/node.tar.xz
-tar xf $INSTALL_DIR/tmp/node.tar.xz -C $INSTALL_DIR/tmp/
-cp $INSTALL_DIR/tmp/node-v5.10.1-linux-x64/bin/node $INSTALL_DIR/node
-rm -rf $INSTALL_DIR/tmp/
-
-# Enclose (needs to do npm install -g enclose)
-#enclose PGS.js
-#jx package PGS.js PGS -native
-#rm $BASE_DIR/PGS.jxp
+#Check cache
+NODE_CACHE=$BASE_DIR/build/cache/$BUILD_OS
+if [ ! -d "$NODE_CACHE" ]; then
+    echo "Downloading and caching node"
+    mkdir -p $NODE_CACHE/tmp/
+    #Curl in and copy it to our main cache
+    curl $NODE_BASE$NODE_TAR.tar.xz > $NODE_CACHE/tmp/node.tar.xz
+    #Untar it
+    tar xf $NODE_CACHE/tmp/node.tar.xz -C $NODE_CACHE/tmp/
+    #Copy the binary to cache
+    cp $NODE_CACHE/tmp/$NODE_TAR/bin/node $NODE_CACHE/node
+    #Remove tmp dir
+    rm $NODE_CACHE/tmp/ -rf
+fi
+#Copy it to install
+cp $NODE_CACHE/node $INSTALL_DIR/node
 
 #Copy it
 cp $BASE_DIR/PGS.js $INSTALL_DIR/PGS.js
-#cp $BASE_DIR/PGS $INSTALL_DIR/PGS
-#Remove compiled file
-#rm ./PGS
-#Go to head folder
-#cd $INSTALL_DIR/../
