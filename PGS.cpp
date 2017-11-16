6a5e1423c7abb2b76b55ba8ca0e546c9b137f139
CadeBrown <brown.cade@gmail.com>
2017-11-16 18:15:16 -0500

    cpp version started

---
+++ PGS.cpp
@@ -0,0 +1,36 @@
+/*
+ * PGS.cpp -- the controlling interface for PGS
+ *
+ * C ChemicalDevelopment 2017
+ *   Part of the PGS project.
+ *   <http://pgs.chemicaldevelopment.us/>
+ *
+ * Authors:
+ *   Cade Brown <cade@cade.site>
+ *
+ * PGS is free software; you can redistribute it and/or modify 
+ * it under the terms of the GNU GPL v3 or later, at your choice. 
+ * See details in LICENSE.txt
+ *
+ */
+
+
+#include <iostream>
+using namespace std;
+
+#include "PGSConfig.hpp"
+
+int main(int argc, char ** argv) {
+
+
+    cout << "PGS v" << PGS_VERSION_MAJOR << "." << PGS_VERSION_MINOR << endl;
+
+    // print out how many arguments there are
+    cout << "arguments: " << argc << endl;
+    
+
+    return 0;
+}
+
+
+
